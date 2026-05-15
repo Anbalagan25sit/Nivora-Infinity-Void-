604fa28b1c9b0982f5d94ddfe03e0d905fcf259d
AnbuInfin <anbucyb@gmail.com>
2026-05-15 20:28:59 +0530

    Clean up legacy Nivora folders

--- Nivora-Ver-loop-main/run_commands.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd C:\Users\Nivorichi\Downloads\Nivora-Ver-loop-main\Nivora-Ver-loop-main
-.\venv\Scripts\python.exe run_checks.py > output.txt 2>&1
-type output.txt
--- Nivora-Ver-loop-main/spotify.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Spotify Control Script Wrapper for Windows
-REM Usage: spotify [command] [options]
-
-python "%~dp0spotify_control.py" %*
